6b19f541eb5a119f253fc6f2b48c83427d97ba7d
Swassie <swaschan0013@gmail.com>
2023-07-03 02:24:46 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1> we're fucked</h1>
+      <h1>were fucked</h1>
     </main>
   )
 }
